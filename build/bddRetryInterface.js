5af423658e96f59b0ec6278337feed31539d664c
CTAPbIuMABP <trejgun@gmail.com>
2016-01-12 18:25:22 +0800

    build: es6 + lint

--- build/bddRetryInterface.js
+++ build/bddRetryInterface.js
@@ -1,104 +1,107 @@
-'use strict';
+"use strict";
 
-(function () {
-  var createInterface;
+var _mocha = require("mocha");
 
-  module.exports = createInterface = function createInterface(Mocha) {
-    var RetryTest, Suite, interfaces, patchMochaSuite, utils;
-    RetryTest = require('./retryTest');
-    interfaces = Mocha.interfaces;
-    Suite = Mocha.Suite;
-    utils = Mocha.utils;
-    patchMochaSuite = require('./retrySuite');
-    patchMochaSuite(Mocha);
-    return interfaces.bddretry = function (suite) {
-      var suites;
-      suites = [suite];
-      return suite.on("pre-require", function (context, file, mocha) {
-        context.before = function (times, name, fn) {
-          return suites[0].beforeAllWithRetry(times, name, fn);
-        };
-        context.after = function (times, name, fn) {
-          return suites[0].afterAllWithRetry(times, name, fn);
-        };
-        context.beforeEach = function (times, name, fn) {
-          return suites[0].beforeEachWithRetry(times, name, fn);
-        };
-        context.afterEach = function (times, name, fn) {
-          return suites[0].afterEachWithRetry(times, name, fn);
-        };
-        context.describe = context.context = function (times, title, fn) {
-          var asuite, ref;
-          if (fn == null) {
-            ref = [times, title], title = ref[0], fn = ref[1];
-            times = void 0;
-          }
-          if (suites[0].times != null && times == null) {
-            times = suites[0].times;
-          }
-          if (context.DEFAULT_RETRY != null && times == null) {
-            times = context.DEFAULT_RETRY;
-          }
-          asuite = Suite.create(suites[0], title);
-          asuite.times = times;
-          asuite.file = file;
-          suites.unshift(asuite);
-          fn.call(asuite);
-          suites.shift();
-          return asuite;
-        };
-        context.xdescribe = context.xcontext = context.describe.skip = function (times, title, fn) {
-          var asuite, ref;
-          if (fn == null) {
-            ref = [times, title], title = ref[0], fn = ref[1];
-            times = void 0;
-          }
-          asuite = Suite.create(suites[0], title);
-          asuite.pending = true;
-          suites.unshift(asuite);
-          fn.call(asuite);
-          return suites.shift();
-        };
-        context.describe.only = function (times, title, fn) {
-          var asuite;
-          asuite = context.describe(times, title, fn);
-          mocha.grep(asuite.fullTitle());
-          return asuite;
-        };
-        context.it = context.itretry = function (times, title, fn) {
-          var asuite, ref, test;
-          asuite = suites[0];
-          if (fn == null && typeof times !== 'number') {
-            ref = [times, title], title = ref[0], fn = ref[1];
-            times = asuite.times != null ? asuite.times : 1;
-          }
-          if (asuite.pending) {
-            fn = null;
-          }
-          test = new RetryTest(times, title, fn);
-          test.file = file;
-          asuite.addTest(test);
-          return test;
-        };
-        context.it.only = function (times, title, fn) {
-          var reString, test;
-          test = context.it(times, title, fn);
-          reString = "^" + utils.escapeRegexp(test.fullTitle()) + "$";
-          mocha.grep(new RegExp(reString));
-          return test;
-        };
-        return context.xit = context.xspecify = context.it.skip = function (times, title, fn) {
-          var ref;
-          if (title == null) {
-            return context.it(times);
-          }
-          if (fn == null) {
-            ref = [1, times, title], times = ref[0], title = ref[1], fn = ref[2];
-          }
-          return context.it(times, title);
-        };
-      });
+var _retryTest = require("./retryTest");
+
+var _retryTest2 = _interopRequireDefault(_retryTest);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = function createInterface() {
+
+    return function (suite) {
+        var suites = [suite];
+        suite.on("pre-require", function (context, file, mocha) {
+            context.before = function () {
+                var _suites$;
+
+                return (_suites$ = suites[0]).beforeAllWithRetry.apply(_suites$, arguments);
+            };
+            context.after = function () {
+                var _suites$2;
+
+                return (_suites$2 = suites[0]).afterAllWithRetry.apply(_suites$2, arguments);
+            };
+            context.beforeEach = function () {
+                var _suites$3;
+
+                return (_suites$3 = suites[0]).beforeEachWithRetry.apply(_suites$3, arguments);
+            };
+            context.afterEach = function () {
+                var _suites$4;
+
+                return (_suites$4 = suites[0]).afterEachWithRetry.apply(_suites$4, arguments);
+            };
+            context.describe = context.context = function (times, title, fn) {
+                if (!fn) {
+                    var _ref = [void 0, times, title];
+                    times = _ref[0];
+                    title = _ref[1];
+                    fn = _ref[2];
+                }
+                times = times || suites[0].times || context.DEFAULT_RETRY;
+                var asuite = _mocha.Suite.create(suites[0], title);
+                asuite.times = times;
+                asuite.file = file;
+                suites.unshift(asuite);
+                fn.call(asuite);
+                suites.shift();
+                return asuite;
+            };
+            context.xdescribe = context.xcontext = context.describe.skip = function (times, title, fn) {
+                if (!fn) {
+                    var _ref2 = [void 0, times, title];
+                    times = _ref2[0];
+                    title = _ref2[1];
+                    fn = _ref2[2];
+                }
+                var asuite = _mocha.Suite.create(suites[0], title);
+                asuite.pending = true;
+                suites.unshift(asuite);
+                fn.call(asuite);
+                return suites.shift();
+            };
+            context.describe.only = function (times, title, fn) {
+                var asuite = context.describe(times, title, fn);
+                mocha.grep(asuite.fullTitle());
+                return asuite;
+            };
+            context.it = context.itretry = function (times, title, fn) {
+                var asuite = suites[0];
+                if (!fn && typeof times !== "number") {
+                    var _ref3 = [asuite.times || 1, times, title];
+                    times = _ref3[0];
+                    title = _ref3[1];
+                    fn = _ref3[2];
+                }
+                if (asuite.pending) {
+                    fn = null;
+                }
+                var test = new _retryTest2.default(times, title, fn);
+                test.file = file;
+                asuite.addTest(test);
+                return test;
+            };
+            context.it.only = function (times, title, fn) {
+                var test = context.it(times, title, fn);
+                var reString = "^" + _mocha.utils.escapeRegexp(test.fullTitle()) + "$";
+                mocha.grep(new RegExp(reString));
+                return test;
+            };
+            context.xit = context.xspecify = context.it.skip = function (times, title, fn) {
+                if (!title) {
+                    return context.it(times);
+                }
+                if (!fn) {
+                    var _ref4 = [1, times, title];
+                    times = _ref4[0];
+                    title = _ref4[1];
+                    fn = _ref4[2];
+                }
+                return context.it(times, title);
+            };
+        });
     };
-  };
-}).call(undefined);
+};
 //# sourceMappingURL=bddRetryInterface.js.map
--- source/bddRetryInterface.js
+++ source/bddRetryInterface.js
@@ -1,102 +1,74 @@
-(function() {
-  var createInterface;
+"use strict";
 
-  module.exports = createInterface = function(Mocha) {
-    var RetryTest, Suite, interfaces, patchMochaSuite, utils;
-    RetryTest = require('./retryTest');
-    interfaces = Mocha.interfaces;
-    Suite = Mocha.Suite;
-    utils = Mocha.utils;
-    patchMochaSuite = require('./retrySuite');
-    patchMochaSuite(Mocha);
-    return interfaces.bddretry = function(suite) {
-      var suites;
-      suites = [suite];
-      return suite.on("pre-require", function(context, file, mocha) {
-        context.before = function(times, name, fn) {
-          return suites[0].beforeAllWithRetry(times, name, fn);
-        };
-        context.after = function(times, name, fn) {
-          return suites[0].afterAllWithRetry(times, name, fn);
-        };
-        context.beforeEach = function(times, name, fn) {
-          return suites[0].beforeEachWithRetry(times, name, fn);
-        };
-        context.afterEach = function(times, name, fn) {
-          return suites[0].afterEachWithRetry(times, name, fn);
-        };
-        context.describe = context.context = function(times, title, fn) {
-          var asuite, ref;
-          if (fn == null) {
-            ref = [times, title], title = ref[0], fn = ref[1];
-            times = void 0;
-          }
-          if ((suites[0].times != null) && (times == null)) {
-            times = suites[0].times;
-          }
-          if ((context.DEFAULT_RETRY != null) && (times == null)) {
-            times = context.DEFAULT_RETRY;
-          }
-          asuite = Suite.create(suites[0], title);
-          asuite.times = times;
-          asuite.file = file;
-          suites.unshift(asuite);
-          fn.call(asuite);
-          suites.shift();
-          return asuite;
-        };
-        context.xdescribe = context.xcontext = context.describe.skip = function(times, title, fn) {
-          var asuite, ref;
-          if (fn == null) {
-            ref = [times, title], title = ref[0], fn = ref[1];
-            times = void 0;
-          }
-          asuite = Suite.create(suites[0], title);
-          asuite.pending = true;
-          suites.unshift(asuite);
-          fn.call(asuite);
-          return suites.shift();
-        };
-        context.describe.only = function(times, title, fn) {
-          var asuite;
-          asuite = context.describe(times, title, fn);
-          mocha.grep(asuite.fullTitle());
-          return asuite;
-        };
-        context.it = context.itretry = function(times, title, fn) {
-          var asuite, ref, test;
-          asuite = suites[0];
-          if ((fn == null) && typeof times !== 'number') {
-            ref = [times, title], title = ref[0], fn = ref[1];
-            times = asuite.times != null ? asuite.times : 1;
-          }
-          if (asuite.pending) {
-            fn = null;
-          }
-          test = new RetryTest(times, title, fn);
-          test.file = file;
-          asuite.addTest(test);
-          return test;
-        };
-        context.it.only = function(times, title, fn) {
-          var reString, test;
-          test = context.it(times, title, fn);
-          reString = "^" + utils.escapeRegexp(test.fullTitle()) + "$";
-          mocha.grep(new RegExp(reString));
-          return test;
-        };
-        return context.xit = context.xspecify = context.it.skip = function(times, title, fn) {
-          var ref;
-          if (title == null) {
-            return context.it(times);
-          }
-          if (fn == null) {
-            ref = [1, times, title], times = ref[0], title = ref[1], fn = ref[2];
-          }
-          return context.it(times, title);
-        };
-      });
+import {utils, Suite} from "mocha";
+import RetryTest from "./retryTest";
+
+
+module.exports = function createInterface() {
+
+    return (suite) => {
+        const suites = [suite];
+        suite.on("pre-require", (context, file, mocha) => {
+            context.before = (...args) => suites[0].beforeAllWithRetry(...args);
+            context.after = (...args) => suites[0].afterAllWithRetry(...args);
+            context.beforeEach = (...args) => suites[0].beforeEachWithRetry(...args);
+            context.afterEach = (...args) => suites[0].afterEachWithRetry(...args);
+            context.describe = context.context = (times, title, fn) => {
+                if (!fn) {
+                    [times, title, fn] = [void 0, times, title];
+                }
+                times = times || suites[0].times || context.DEFAULT_RETRY;
+                const asuite = Suite.create(suites[0], title);
+                asuite.times = times;
+                asuite.file = file;
+                suites.unshift(asuite);
+                fn.call(asuite);
+                suites.shift();
+                return asuite;
+            };
+            context.xdescribe = context.xcontext = context.describe.skip = (times, title, fn) => {
+                if (!fn) {
+                    [times, title, fn] = [void 0, times, title];
+                }
+                const asuite = Suite.create(suites[0], title);
+                asuite.pending = true;
+                suites.unshift(asuite);
+                fn.call(asuite);
+                return suites.shift();
+            };
+            context.describe.only = (times, title, fn) => {
+                const asuite = context.describe(times, title, fn);
+                mocha.grep(asuite.fullTitle());
+                return asuite;
+            };
+            context.it = context.itretry = (times, title, fn) => {
+                const asuite = suites[0];
+                if (!fn && typeof times !== "number") {
+                    [times, title, fn] = [asuite.times || 1, times, title];
+                }
+                if (asuite.pending) {
+                    fn = null;
+                }
+                const test = new RetryTest(times, title, fn);
+                test.file = file;
+                asuite.addTest(test);
+                return test;
+            };
+            context.it.only = (times, title, fn) => {
+                const test = context.it(times, title, fn);
+                const reString = "^" + utils.escapeRegexp(test.fullTitle()) + "$";
+                mocha.grep(new RegExp(reString));
+                return test;
+            };
+            context.xit = context.xspecify = context.it.skip = (times, title, fn) => {
+                if (!title) {
+                    return context.it(times);
+                }
+                if (!fn) {
+                    [times, title, fn] = [1, times, title];
+                }
+                return context.it(times, title);
+            };
+        });
     };
-  };
-
-}).call(this);
+};
